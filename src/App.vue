f133b6d27ec7385825711073ece94bd7cbeb74bc
Rafael Dias <rdias.ga@gmail.com>
2020-07-21 00:00:49 -0300

    Improves the style of the plugin

--- src/App.vue
+++ src/App.vue
@@ -37,4 +37,73 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  font: 12px sans-serif;
+  margin: 20px;
+}
+
+button {
+  border-radius: 5px;
+  background: white;
+  color: black;
+  border: none;
+  padding: 8px 15px;
+  margin: 0 5px;
+  box-shadow: inset 0 0 0 1px black;
+  outline: none;
+}
+
+#create {
+  box-shadow: none;
+  background: #18a0fb;
+  color: white;
+}
+
+input[type=number] {
+    display: block;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    outline-color: transparent !important;
+}
+
+input[type=number]:hover {
+  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
+}
+
+button:focus {
+  box-shadow: inset 0 0 0 2px #18a0fb;
+}
+
+#create:focus {
+  box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.3);
+}
+
+input[type=number]:focus {
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+}
+
+.input-group {
+  margin: 16px 0px;
+}
 </style>
