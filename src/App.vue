882fc9b2588c8de0fbf9d9199f92513d191ddf3b
Rafael Dias <rdias.ga@gmail.com>
2020-07-20 23:58:12 -0300

    Implements the feature to create a stepper specifying the number of steps

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
+  <div>
+    <h2>Stepper Creator</h2>
+    <div class="input-group">
+      <label>Number of steps: </label>
+      <input
+        type="number"
+        v-model.number="number_of_steps"
+        value="3"
+      />
+    </div>
+    <button @click="cancel">Cancel</button>
+    <button id="create" @click="create">Create</button>
+  </div>
 </template>
 
 <script>
+export default {
+  name: "App",
+  data() {
+    return {
+      number_of_steps: 3,
+    };
+  },
+  methods: {
+    create: function() {
+      const number_of_steps = parseInt(this.number_of_steps, 10);
+
+      parent.postMessage({ pluginMessage: { type: 'create-steps', number_of_steps, } }, '*')
+    },
+    cancel: function() {
+      parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+    }
+  },
+  created() {}
+};
 </script>
 
 <style>
--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,64 @@
 figma.showUI(__html__);
+
+function clone(val) {
+	return JSON.parse(JSON.stringify(val))
+}
+
+figma.ui.onmessage = data => {
+	if (data.type === 'create-steps') {
+		const nodes: SceneNode[] = [];
+		for (let i = 0; i < data.number_of_steps; i++) {
+			const circle = figma.createEllipse();
+			circle.resize(28, 28);
+			circle.x = (i * 28) + (48 * i);;
+			circle.fills = [{ type: 'SOLID', color: { r: 0.41569, g: 0.45882, b: 0.50196 } }];
+
+			figma.currentPage.appendChild(circle);
+			nodes.push(circle);
+
+			figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+				.then(() => {
+					const text = figma.createText();
+					text.characters = '' + (i + 1);
+
+					text.x = ((28 - text.width) / 2)
+						+ ((48 * i)
+							+ (28 * i));
+
+					text.y = ((28 - text.height) / 2);
+
+					const fills = clone(text.fills);
+					fills[0].color = { r: 1, g: 1, b: 1 };
+					text.fills = fills;
+
+					figma.currentPage.appendChild(text);
+					nodes.push(text);
+				});
+
+			if (i !== data.number_of_steps - 1) {
+				const line = figma.createLine();
+
+				if (data.direction === 'vertical') {
+					line.y = ((i + 1) * 28) + (48 * i);
+					line.x = 28 / 2;
+					line.rotation = -90;
+				} else {
+					line.x = ((i + 1) * 28) + (48 * i);
+					line.y = 28 / 2;
+				}
+
+				line.strokes = [{ type: 'SOLID', color: { r: 0.80784, g: 0.83137, b: 0.85490 } }];
+				line.resize(48, 0);
+
+				figma.currentPage.appendChild(line);
+				nodes.push(line);
+			}
+
+		}
+
+		figma.currentPage.selection = nodes;
+		figma.viewport.scrollAndZoomIntoView(nodes);
+	}
+
+	figma.closePlugin();
+};
